2cb835ee8d274fe8776d497fbeb9e266264155f7
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-06-29 15:48:55 -0500

    factory de modelos y llamamos en el DataBaseSeeder para tener datos aleatorios en base de datos

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,15 +10,59 @@
 | database. Just tell the factory how a default model should look.
 |
 */
-
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+
+use App\User;
+use App\Seller;
+use App\Product;
+use App\Category;
+use App\Transaction;
+
+$factory->define(User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'verified' => $verificado = $faker->randomElement([User::USUARIO_VERIFICADO, User::USUARIO_NO_VERIFICADO]),
+        'verification_token' => $verificado == User::USUARIO_VERIFICADO? null : User::generarVerificationToken(),
+        'admin' => $faker->randomElement([User::USUARIO_ADMINITRADOR, User::USUARIO_NO_ADMINITRADOR]),
         'remember_token' => str_random(10),
+    ];
+});
+
+#Factory para Category
+$factory->define(Category::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+#Factory para Product
+$factory->define(Product::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantify' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['bebidas.png', 'pringles.png', 'shampoo.png']),
+        'seller_id' => User::all()->random()->id,
+    ];
+});
+
+#Factory para Transaction
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+
+    $vendedor = Seller::has('products')->get()->random();
+    $comprador = User::all()->except($vendedor->id)->random();
+
+    return [
+        'quantify' => $faker->numberBetween(1,3),
+        'buyer_id' => $comprador->id,
+        'product_id' => $vendedor->products->random()->id,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
-
+#llamamos a modelos, opcional
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -10,7 +15,25 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    {	
+    	#DESACTIVAR CLAVE FORANEA
+    	DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        #borrar datos sin tabla
+        User::truncate();
+    	Category::truncate();
+    	Product::truncate();
+    	Transaction::truncate();
+    	DB::table('category_product')->truncate();
+
+    	factory(User::class, 200)->create();
+    	factory(Category::class, 30)->create();
+
+    	#relacion muchos a muchos
+    	factory(Product::class, 1000)->create()->each(function($producto){
+    		$categorias = Category::all()->random(mt_rand(1, 5))->pluck('id');
+    		$producto->categories()->attach($categorias);
+    	});
+
+    	factory(Transaction::class, 1000)->create();
     }
 }
