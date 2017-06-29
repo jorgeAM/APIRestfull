8aba5945823521a17c7c3d71fb91f5a623fd469f
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-06-29 15:29:59 -0500

    migracion de tabla products

--- database/migrations/2017_06_29_185715_create_products_table.php
+++ database/migrations/2017_06_29_185715_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,6 +16,16 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            #atributos agregados por nosotros
+            $table->string('name');
+            $table->string('description', 500);
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            #relacion
+            $table->integer('seller_id')->unsigned();
+            $table->foreign('seller_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
