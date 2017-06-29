51e7eaf29e71c42d2d0e440ccfd0b00596113a0a
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-06-29 15:35:37 -0500

    migracion de tabla transactions

--- database/migrations/2017_06_29_185800_create_transactions_table.php
+++ database/migrations/2017_06_29_185800_create_transactions_table.php
@@ -15,6 +15,14 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
+            #atributos agregados por nosotros
+            $table->integer('quantity')->unsigned();
+            #relaciones
+            $table->integer('buyer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
+
             $table->timestamps();
         });
     }
