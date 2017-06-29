825b5513bd87d9d7bb5573b77b1fd4ae5b1c1078
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-06-29 14:46:41 -0500

    implementamos modelo User

--- app/User.php
+++ app/User.php
@@ -7,24 +7,49 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {   
-    use Notifiable;
-    protected $table = 'users';
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  const USUARIO_VERIFICADO = '1';
+  const USUARIO_NO_VERIFICADO = '0';
+  const USUARIO_ADMINISTRADOR = 'true';
+  const USUARIO_NO_ADMINISTRADOR = 'false';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  protected $table = 'users';
+
+  /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+  */
+
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'verified',
+    'verification_token',
+    'admin',
+  ];
+
+  /**
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+  */
+  
+  protected $hidden = [
+    'password', 'remember_token', 'verification_token',
+  ];
+
+  public function esVerificado(){
+    return $this->verified == User::USUARIO_VERIFICADO;
+  }
+
+  public function esAdministrador(){
+    return $this->admin == User::USUARIO_ADMINISTRADOR;
+  }
+
+  public static function generarVerificationToken(){
+    return str_random(40);
+  }
 }
