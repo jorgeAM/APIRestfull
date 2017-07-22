0c596fda850c0c193032059c4da9d9e63d2d7b7c
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-07-21 20:22:15 -0500

    categorias que compro un comprador, usamos metodo collapse, pues nos sacaba categoria por CADA PRODUCTO y con ese metodo todo lo juntamos

---
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        #varias listas de categories
+        $categories = $buyer->transactions()->with('product.categories')->get()->pluck('product.categories')
+        #para que se junten todo en 1
+        ->collapse()
+        ->unique('id')
+        ->values();
+        return $this->showAll($categories);
+    }
+
+}
