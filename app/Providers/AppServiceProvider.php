5d0a53eb1a842ba7e3bf0180f7629989991531ab
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-02 23:21:56 -0500

    cuando no hay stock automaticamente cambia el status del proyecto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,8 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        #para el error de utf8mb4
         Schema::defaultStringLength(191);
+        #cuando ya no hay stock que cambie a NO DISPONIBLE automaticamente
+        Product::updated(function($product){
+            if($product->quantity == 0 && $product->estaDisponible()){
+                $product->status = Product::PRODUCTO_NO_DISPONIBLE;
+                $product->save();
+            }
+        });
     }
 
     /**
