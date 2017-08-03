2ed0963c0eb0647623260540f97e6d7c38a1d01d
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-02 23:11:55 -0500

    agregar un producto a una transaccion que realiza un comprador

---
+++ app/Http/Controllers/Product/ProductBuyerTransactionController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\User;
+use App\Product;
+use App\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ApiController;
+
+class ProductBuyerTransactionController extends ApiController
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Product $product, User $buyer)
+    {
+        #validación
+        $this->validate($request, [
+            'quantity' => 'required|integer|min:1'
+        ]);
+
+        #ver que el comprador y el vendedor no sean la misma persona
+        if($buyer->id == $product->seller_id){
+            return $this->errorResponse('Crrano no puedes comprar tu propio producto.', 409);
+        }
+        #comprador debe estar verificado
+        if(!$buyer->esVerificado()){
+            return $this->errorResponse('Crrano debes verificar la cuenta, si quieres comprar.', 409);
+        }
+        #usuario debe estar verificado
+        if(!$product->seller->esVerificado()){
+            return $this->errorResponse('El Crrano que te vende debes verificar la cuenta.', 409);
+        }
+        #producto disponible
+        if(!$product->estaDisponible()){
+            return $this->errorResponse('Producto no disponible', 409);
+        }
+        #si se quiere comprar un cantidad mayor a la que hay en stock
+        if($product->quantity < $request->quantity){
+            return $this->errorResponse('Crrano, no hay mucho stock!!', 409);
+        }
+        #en caso de que se quiera hacer varias transacciones con el mismo producto a la vez, si falla en algo
+        #NO PASA NADA y todo sigue igual
+        return DB::transaction(function() use ($request, $product, $buyer){
+            #reducimos de 1 la cantidad
+            $product->quantity -= $request->quantity;
+            #guardamos
+            $product->save();
+            $transaction = Transaction::create([
+                'quantity' => $request->quantity,
+                'buyer_id' => $buyer->id,
+                'product_id' => $product->id
+            ]);
+            #retornamos lo que agregamos
+            return $this->showOne($transaction, 201);
+        });
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::resource('categories.transactions', 'Category\CategoryTransactionControll
 #products
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.buyers.transactions', 'Product\ProductBuyerTransactionController', ['only' => ['store']]);
 Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
 #transactions
