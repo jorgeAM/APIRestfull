cb669e3a811d294d34df3d28dfd511ea7eaa50b3
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-07 16:28:40 -0500

    implementamos el evento para que se envie el correo al actualizar el email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use App\User;
 use App\Product;
 use App\Mail\UserCreated;
+use App\Mail\UserMailChanged;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -19,11 +20,18 @@ class AppServiceProvider extends ServiceProvider
     {
         #para el error de utf8mb4
         Schema::defaultStringLength(191);
-
         #cuando un usuario es creado
         User::created(function($user){
             #enviamos correo
             Mail::to($user)->send(new UserCreated($user));
+        });
+        #cuando un usuario es actualizado
+        User::updated(function($user){
+            #asegurarnos que el email haya sido modificado
+            if($user->isDirty('email')){
+              #enviamos correo
+              Mail::to($user)->send(new UserMailChanged($user));
+            }
         });
 
         #cuando ya no hay stock que cambie a NO DISPONIBLE automaticamente
