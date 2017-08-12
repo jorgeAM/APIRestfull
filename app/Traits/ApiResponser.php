358dab0da8b04af7ac4b030bd2412eaf64c021a6
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-12 18:55:19 -0500

    REVISAR: paginación

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser{
 	private function successResponse($data, $code){
@@ -23,6 +25,8 @@ trait ApiResponser{
 		$transformer = $collection->first()->transformer;
 		#ordenamos
 		$collection = $this->sortData($collection, $transformer);
+		#hacemos la paginación
+		$collection = $this->paginate($collection);
 		#transformamos
 		$collection = $this->transformData($collection, $transformer);
 		return $this->successResponse($collection, $code);
@@ -51,6 +55,30 @@ trait ApiResponser{
 			$collection = $collection->sortBy->{$attribute};
 		}
 		return $collection;
+	}
+
+	#metodo que hará la paginación
+	protected function paginate(Collection $collection){
+		#reglas para cuando el usuario elija cuantos elementos por pagina
+		$rules = [
+			'per_page' => 'integer|min:2|max:50'
+		];
+		Validator::validate(request()->all(), $rules);
+		#metodo propio de laravel
+		$page = LengthAwarePaginator::resolveCurrentPage();
+		#elemenos por pagina
+		$perPage = 15;
+		if(request()->has('per_page')){
+			$perPage = (int)request()->per_page;
+		}
+		#resultado de cada pagina
+		$results = $collection->slice(($page -1)*$perPage, $perPage)->values();
+
+		$paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+			'path' => LengthAwarePaginator::resolveCurrentPath()
+		]);
+		$paginated->appends(request()->all());
+		return $paginated;
 	}
 
 	#metodo para transformar
