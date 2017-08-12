1525cdc358d3c09051f504ed14b57bcb7213e777
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-11 23:03:59 -0500

    ProductTransformer

---
+++ app/Transformers/ProductTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class ProductTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Product $product)
+    {
+        return [
+            #tranformación(el tipo de dato)
+            'identificador' => (int)$product->id,
+            'titulo' => (string)$product->name,
+            'detaller' => (string)$product->description,
+            'disponibles' => (int)$product->quantity,
+            'estado' => (string)$product->status,
+            #sera un URL
+            'imagen' => url('img/{$product->image}'),
+            'vendedor' => (int)$product->seller_id,
+            'fechaCreación' => (string)$product->created_at,
+            'fechaActualización' => (string)$product->updated_at,
+            'fechaEliminación' => isset($product->deleted_at) ? (string)$product->deleted_at : null
+        ];
+    }
+}
