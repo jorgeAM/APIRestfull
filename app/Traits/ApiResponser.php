60aee3d7fe873d7df664693ba8a9db64e89289dd
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-12 23:44:43 -0500

    REVISAR 2.0: Cache

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -1,104 +1,93 @@
 <?php
-
 namespace App\Traits;
-
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
-
-trait ApiResponser{
-	private function successResponse($data, $code){
+trait ApiResponser
+{
+	private function successResponse($data, $code)
+	{
 		return response()->json($data, $code);
 	}
-
-	protected function errorResponse($message, $code){
-		return response()->json(['error' => $message, 'code' => $code], $code);	
+	protected function errorResponse($message, $code)
+	{
+		return response()->json(['error' => $message, 'code' => $code], $code);
 	}
-
-	protected function showAll(Collection $collection, $code=200){
-		if($collection->isEmpty()){
-			#no transformamos nada, porque no hay nada
-			return $this->successResponse(['data' => $collection, 'code' => 200]);
+	protected function showAll(Collection $collection, $code = 200)
+	{
+		if ($collection->isEmpty()) {
+			return $this->successResponse(['data' => $collection], $code);
 		}
-		#conseguimos el transformador
 		$transformer = $collection->first()->transformer;
-		#ordenamos
+		$collection = $this->filterData($collection, $transformer);
 		$collection = $this->sortData($collection, $transformer);
-		#hacemos la paginación
 		$collection = $this->paginate($collection);
-		#transformamos
 		$collection = $this->transformData($collection, $transformer);
-		#
 		$collection = $this->cacheResponse($collection);
-		#damos la respuesta
 		return $this->successResponse($collection, $code);
 	}
-
-	protected function showOne(Model $instance, $code=200){
-		#sacamos el transformer
+	protected function showOne(Model $instance, $code = 200)
+	{
 		$transformer = $instance->transformer;
-		#transformamos
 		$instance = $this->transformData($instance, $transformer);
-		#retornamos
 		return $this->successResponse($instance, $code);
 	}
-
-	protected function showMessage($message, $code=200){
+	protected function showMessage($message, $code = 200)
+	{
 		return $this->successResponse(['data' => $message], $code);
 	}
-
-	#metodo para ordenar
-	protected function sortData(Collection $collection, $transformer){
-		#
-		if(request()->has('sort_by')){
-			#el metodo es estatico, se llama ::
-			$attribute = $transformer::originalAttribute(request()->sort_by);			
-			#$collection = $collection->sortBy($attribute);
+	protected function filterData(Collection $collection, $transformer)
+	{
+		foreach (request()->query() as $query => $value) {
+			$attribute = $transformer::originalAttribute($query);
+			if (isset($attribute, $value)) {
+				$collection = $collection->where($attribute, $value);
+			}
+		}
+		return $collection;
+	}
+	protected function sortData(Collection $collection, $transformer)
+	{
+		if (request()->has('sort_by')) {
+			$attribute = $transformer::originalAttribute(request()->sort_by);
 			$collection = $collection->sortBy->{$attribute};
 		}
 		return $collection;
 	}
-
-	#metodo que hará la paginación
-	protected function paginate(Collection $collection){
-		#reglas para cuando el usuario elija cuantos elementos por pagina
+	protected function paginate(Collection $collection)
+	{
 		$rules = [
 			'per_page' => 'integer|min:2|max:50'
 		];
 		Validator::validate(request()->all(), $rules);
-		#metodo propio de laravel
 		$page = LengthAwarePaginator::resolveCurrentPage();
-		#elemenos por pagina
 		$perPage = 15;
-		if(request()->has('per_page')){
-			$perPage = (int)request()->per_page;
+		if (request()->has('per_page')) {
+			$perPage = (int) request()->per_page;
 		}
-		#resultado de cada pagina
-		$results = $collection->slice(($page -1)*$perPage, $perPage)->values();
-
+		$results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
 		$paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
-			'path' => LengthAwarePaginator::resolveCurrentPath()
+			'path' => LengthAwarePaginator::resolveCurrentPath(),
 		]);
 		$paginated->appends(request()->all());
 		return $paginated;
 	}
-
-	#metodo para transformar
-	protected function transformData($data, $transformer){
-		#transformamos y pasamos a array
-		$transformation = fractal($data, new $transformer)->toArray();
-		#retornamos el array
-		return $transformation;
+	protected function transformData($data, $transformer)
+	{
+		$transformation = fractal($data, new $transformer);
+		return $transformation->toArray();
 	}
-
-	protected function cacheResponse($data){
-		#url actual
+	protected function cacheResponse($data)
+	{
 		$url = request()->url();
-		return Cache::remember($url, 30/60, function() use ($data){
+		$queryParams = request()->query();
+		ksort($queryParams);
+		$queryString = http_build_query($queryParams);
+		$fullUrl = "{$url}?{$queryString}";
+		return Cache::remember($fullUrl, 15/60, function() use($data) {
 			return $data;
 		});
 	}
-
 }
