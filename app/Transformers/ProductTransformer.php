b50da4785a7670ba3c61caa4334d82f9aff85920
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-20 18:22:15 -0500

    hateOS en ProductTransformer

--- app/Transformers/ProductTransformer.php
+++ app/Transformers/ProductTransformer.php
@@ -15,18 +15,41 @@ class ProductTransformer extends TransformerAbstract
     public function transform(Product $product)
     {
         return [
-            #tranformación(el tipo de dato)
-            'identificador' => (int)$product->id,
-            'titulo' => (string)$product->name,
-            'detalle' => (string)$product->description,
-            'disponibles' => (int)$product->quantity,
-            'estado' => (string)$product->status,
-            #sera un URL
-            'imagen' => url('img/{$product->image}'),
-            'vendedor' => (int)$product->seller_id,
-            'fechaCreación' => (string)$product->created_at,
-            'fechaActualización' => (string)$product->updated_at,
-            'fechaEliminación' => isset($product->deleted_at) ? (string)$product->deleted_at : null
+          #tranformación(el tipo de dato)
+          'identificador' => (int)$product->id,
+          'titulo' => (string)$product->name,
+          'detalle' => (string)$product->description,
+          'disponibles' => (int)$product->quantity,
+          'estado' => (string)$product->status,
+          #sera un URL
+          'imagen' => url('img/{$product->image}'),
+          'vendedor' => (int)$product->seller_id,
+          'fechaCreación' => (string)$product->created_at,
+          'fechaActualización' => (string)$product->updated_at,
+          'fechaEliminación' => isset($product->deleted_at) ? (string)$product->deleted_at : null,
+          #para hateOS
+          'links' => [
+              [
+                'rel' => 'self',
+                'href' => route('products.show', $product->id)
+              ],
+              [
+                'rel' => 'product.buyers',
+                'href' => route('products.buyers.index', $product->id)
+              ],
+              [
+                'rel' => 'product.categories',
+                'href' => route('products.categories.index', $product->id)
+              ],
+              [
+                'rel' => 'product.transactions',
+                'href' => route('products.transactions.index', $product->id)
+              ],
+              [
+                'rel' => 'seller',
+                'href' => route('sellers.show', $product->seller_id)
+              ]
+          ]
         ];
     }
 
