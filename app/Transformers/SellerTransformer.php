e385f5ee8a9b984c0361ddad77f88345962613b9
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-20 18:54:17 -0500

    hateOS en SellerTransformer

--- app/Transformers/SellerTransformer.php
+++ app/Transformers/SellerTransformer.php
@@ -22,7 +22,34 @@ class SellerTransformer extends TransformerAbstract
             'esVerificado' => (int)$seller->verified,
             'fechaCreación' => (string)$seller->created_at,
             'fechaActualización' => (string)$seller->updated_at,
-            'fechaEliminación' => isset($seller->deleted_at) ? (string)$seller->deleted_at : null
+            'fechaEliminación' => isset($seller->deleted_at) ? (string)$seller->deleted_at : null,
+            #para hateOS
+            'links' => [
+              [
+                'rel' => 'self',
+                'href' => route('sellers.show', $seller->id)
+              ],
+              [
+                'rel' => 'seller.buyers',
+                'href' => route('sellers.buyers.index', $seller->id)
+              ],
+              [
+                'rel' => 'seller.categories',
+                'href' => route('sellers.categories.index', $seller->id)
+              ],
+              [
+                'rel' => 'seller.products',
+                'href' => route('sellers.products.index', $seller->id)
+              ],
+              [
+                'rel' => 'seller.transactions',
+                'href' => route('sellers.transactions.index', $seller->id)
+              ],
+              [
+                'rel' => 'user',
+                'href' => route('users.show', $seller->id)
+              ]
+            ]
         ];
     }
 
