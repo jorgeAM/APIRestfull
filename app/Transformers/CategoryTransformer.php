e721cdd5f348bd84a81b6eca93134364534ae302
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-20 18:10:07 -0500

    hateOS en CategoryTransformer

--- app/Transformers/CategoryTransformer.php
+++ app/Transformers/CategoryTransformer.php
@@ -15,13 +15,36 @@ class CategoryTransformer extends TransformerAbstract
     public function transform(Category $category)
     {
         return [
-            #tranformación(el tipo de dato)
-            'identificador' => (int)$category->id,
-            'titulo' => (string)$category->name,
-            'detalles' => (string)$category->description,
-            'fechaCreación' => (string)$category->created_at,
-            'fechaActualización' => (string)$category->updated_at,
-            'fechaEliminación' => isset($category->deleted_at) ? (string)$category->deleted_at : null
+          #tranformación(el tipo de dato)
+          'identificador' => (int)$category->id,
+          'titulo' => (string)$category->name,
+          'detalles' => (string)$category->description,
+          'fechaCreación' => (string)$category->created_at,
+          'fechaActualización' => (string)$category->updated_at,
+          'fechaEliminación' => isset($category->deleted_at) ? (string)$category->deleted_at : null,
+          #para hateOS
+          'links' => [
+              [
+                'rel' => 'self',
+                'href' => route('categories.show', $category->id)
+              ],
+              [
+                'rel' => 'category.buyers',
+                'href' => route('categories.buyers.index', $category->id)
+              ],
+              [
+                'rel' => 'category.products',
+                'href' => route('categories.products.index', $category->id)
+              ],
+              [
+                'rel' => 'category.sellers',
+                'href' => route('categories.sellers.index', $category->id)
+              ],
+              [
+                'rel' => 'category.transactions',
+                'href' => route('categories.transactions.index', $category->id)
+              ],
+          ]
         ];
     }
 
