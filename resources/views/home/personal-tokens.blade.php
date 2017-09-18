47fae97255f7754b4db3f1a5ae5d36b79d62374d
asistemas <asistemas@transportesjoselito.com>
2017-09-18 18:25:03 -0500

    creamos componentes vuejs que vienen con passport

---
+++ resources/views/home/personal-tokens.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <passport-personal-access-tokens>
+        </passport-personal-access-tokens>
+    </div>
+</div>
+@endsection
